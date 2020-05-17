287503816493ef8b50d0df3bde64cd467af985de
Raoux <killian@semifir.com>
2020-05-17 17:28:58 +0200

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: [
+    'standard'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: 'module'
+  },
+  plugins: [
+    '@typescript-eslint'
+  ],
+  rules: {
+    quotes: ["error", "double", "simple"],
+    indent: ["error", 4, 2, 'tab'],
+    "eol-last": 0
+  }
+}
