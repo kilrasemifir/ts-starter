abaf1e151860643ec30c5d2b7fa63d4cf7d5fd4a
Raoux <killian@semifir.com>
2020-05-18 18:38:40 +0200

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("Hello World")
+import express from 'express'
+
+// Creation d'un serveur express
+const server = express()
+
+// definition des routes
+server.get('/', (req, res)=>{res.send("hello world")})
+
+// start server
+server.listen(8080, ()=>console.log( "Serveur start" ))
